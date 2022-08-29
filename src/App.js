af57de11e84491d13bcf04e1bd54040356634209
Chihiro-001 <98835638+Chihiro-001@users.noreply.github.com>
2022-08-29 17:09:08 +0900

    Initial commitment

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dictionary App</h1>
+        <p>What word do you want to search?</p>
+        <button type="button" class="btn btn-primary">
+          Test
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
