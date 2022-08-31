b0068ab6ce507e884b17535c43c8e77c1bc30004
Chihiro-001 <98835638+Chihiro-001@users.noreply.github.com>
2022-08-31 11:04:31 +0900

    Updated my app and added footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
       <header className="App-header">
         <Dictionary />
       </header>
+      <footer>
+        This is coded by Chihiro Ismail and open-sourced on
+        <a href="https://github.com/Chihiro-001/dictionary-app-react">GitHub</a>
+        and is hosted on{" "}
+        <a href="https://incomparable-quokka-38a228.netlify.app">Netlify</a>.
+      </footer>
     </div>
   );
 }
