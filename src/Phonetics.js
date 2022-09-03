ab82117e5747076a31e4ad0e556af92348710f59
Chihiro-001 <98835638+Chihiro-001@users.noreply.github.com>
2022-09-03 15:31:52 +0900

    set if statement for phonetic

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -4,17 +4,21 @@ import "./Phonetics.css";
 export default function Phonetics(props) {
   console.log(props.phonetic);
   // If audio is blank, do not show audio and text.
-  return (
-    <span className="Phonetics">
-      {props.phonetic.text}
-      <a
-        href={props.phonetic.audio}
-        target="_blank"
-        rel="noreferrer"
-        className="phonetics-text ms-2"
-      >
-        Listen 🔊
-      </a>
-    </span>
-  );
+  if (props.phonetic.audio) {
+    return (
+      <span className="Phonetics">
+        {props.phonetic.text}
+        <a
+          href={props.phonetic.audio}
+          target="_blank"
+          rel="noreferrer"
+          className="phonetics-text ms-2"
+        >
+          Listen 🔊
+        </a>
+      </span>
+    );
+  } else {
+    return null;
+  }
 }
